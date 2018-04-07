a3ed2a9bd631855ad7efb7a397e306083e705cb2
Alexei <A@Alexeis-MacBook-Pro-2.local>
2018-04-07 14:03:01 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const bodyParset = require('body-parser');
+
+const app = express();
+
+app.use(bodyParset.json());
+
+let users = [];
+
+app.get('/users/', (req, res) => res.json(users.filter(u => u)));
+
+app.post('/users/', (req, res) => {
+  users.push(req.body);
+  res.send();
+});
+
+app.delete('/users/:id', (req, res) => {
+  const id = req.params.id;
+  users[id] = null;
+  res.send();
+});
+
+app.get('/users/:id', (req, res) => {
+  const id = req.params.id;
+  if (users[id]) {
+    res.json(users[id]);
+  } else {
+    res.status(404);
+    res.send();
+  }
+});
+
+app.put('/users/:id', (req, res) => {
+  const id = req.params.id;
+  if (users[id]) {
+    users[id] = Object.assign(users[id], req.body);
+    res.json(users[id]);
+  } else {
+    res.status(404);
+    res.send();
+  }
+});
+
+app.listen(3000);
